1cf2c54bb81c0122e7f11023db24583d70d27937
Andriano27 <arhipovandrey83@gmail.com>
2025-11-21 18:35:25 +0300

    for stepik_project

---
+++ stepik_org26.py
@@ -0,0 +1,143 @@
+flag = True   # флажок поднят
+while flag:   # пока флажок True, цикл работает
+    text = input("Введите слово (или 'стоп'): ")
+    if text == "стоп":
+        flag = False   # опускаем флажок → цикл остановится
+    else:
+        print("Вы ввели:", text)
+
+total = 1
+while total < 10:
+    num = int(input())
+    total *= num
+    print(total)
+
+num = int(input())
+total = 0
+while abs(num) <= 5:
+    total += num
+    num = int(input())
+print(total)
+
+name = input()
+while len(name) > 3:
+    name = input()
+print(name)
+
+language = 'Русский'
+if language != 'English' != 'Español':
+    print('Язык по умолчанию не является ни английским, ни испанским')
+if language != 'English' != 'Русский':
+    print('Язык по умолчанию не является ни английским, ни русским')
+
+a = 4
+b = -5
+c = 2 * a + b
+d = (a - b) // 4
+e = 3 * (a + b)
+f = a % 5 + b
+g = a * b + 21
+print(c, d, e, f, g)
+
+a = 2.5
+b = 3
+c = b / 2
+d = a ** 2
+e = (2 * a + b) / 2
+f = a // b
+g = (a + 2 * b) % 2
+print(c, d, e, f, g)
+
+for i in range(3):
+    print(0, end='+')
+
+for i in range(2):
+    print('we will')
+print('rock you')
+
+for i in range(4):
+    print(i, end='*')
+
+for i in range(4):
+    j = i + 1
+    print(i, j)
+
+total = 0
+for i in range(3):
+    total = total - i
+    print(total)
+
+total = 0
+for i in range(10):
+    total = total + i
+    if i == 9:
+        total = 1
+print(total)
+
+# Напишите функцию get_middle_point(x1, y1, x2, y2), которая принимает в качестве аргументов координаты
+# концов отрезка (x1; y1), (x2; y2) и возвращает координаты точки являющейся серединой данного отрезка.
+def get_middle_point(x1, y1, x2, y2):
+    a = (x_1 + x_2)/2
+    b = (y_1 + y_2)/2
+    return a, b
+# считываем данные
+x_1, y_1 = int(input()), int(input())
+x_2, y_2 = int(input()), int(input())
+# вызываем функцию
+x, y = get_middle_point(x_1, y_1, x_2, y_2)
+print(x, y)
+
+# Напишите функцию get_circle(radius), которая принимает в качестве аргумента радиус окружности и
+# возвращает два значения: длину окружности и площадь круга, ограниченного данной окружностью.
+import math
+def get_circle(radius):
+    c = 2 * math.pi * radius
+    s = math.pi * (radius ** 2)
+    return c, s
+# считываем данные
+r = float(input())
+# вызываем функцию
+length, square = get_circle(r)
+print(length, square)
+
+from math import pi
+def get_circle(radius):
+    length = 2 * pi * radius
+    area = pi * radius ** 2
+    return length, area
+r = float(input())
+length, square = get_circle(r)
+print(length, square)
+
+# Напишите функцию solve(a, b, c), которая принимает в качестве аргументов три целых числа
+# a, b, c – коэффициенты квадратного уравнения a(x**2)+bx+c=0 и возвращает его корни в порядке возрастания.
+def solve(a, b, c):
+    d = b ** 2 - 4 * a * c
+    x = -(b / (2 * a))
+    x1 = (-b - d ** 0.5) / (2 * a)
+    x2 = (-b + d ** 0.5) / (2 * a)
+    if d > 0:
+        return (min(x1, x2), max(x1, x2))
+    elif d == 0:
+        return x1, x2
+# считываем данные
+a, b, c = int(input()), int(input()), int(input())
+# вызываем функцию
+x1, x2 = solve(a, b, c)
+print(x1, x2)
+
+def solve(a, b, c):
+    d = b ** 2 - 4 * a * c
+    x = -(b / (2 * a))
+    x1 = (-b - d ** 0.5) / (2 * a)
+    x2 = (-b + d ** 0.5) / (2 * a)
+    if d > 0:
+        return (min(x1, x2), max(x1, x2))
+    elif d == 0:
+        return x1, x2
+# считываем данные
+a, b, c = int(input()), int(input()), int(input())
+# вызываем функцию
+x1, x2 = solve(a, b, c)
+print(x1, x2)
+
